5ad35b6e86a7d72a4eb7f24f5a23b24d650501ea
Adam Siegel <tobioatom@gmail.com>
2021-01-03 22:54:56 -0800

    new env var

--- dicebot/main.go
+++ dicebot/main.go
@@ -19,7 +19,7 @@ func main() {
 	// 	json.Unmarshal(dat, &cases)
 	// }
 
-	dg, err := discordgo.New("Bot " + os.Getenv("BOTID"))
+	dg, err := discordgo.New("Bot " + os.Getenv("DICEID"))
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
