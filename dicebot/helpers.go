bca9f94af2c1d8106953ca391afb48157d082b32
Adam Siegel <tobioatom@gmail.com>
2021-03-10 12:30:26 -0800

    updating roles

--- dicebot/helpers.go
+++ dicebot/helpers.go
@@ -64,7 +64,7 @@ func remove(slice []string, s int) []string {
 }
 
 func hasRole(m *discordgo.MessageCreate, id string) bool {
-	fmt.Println("roles ", m.Message.Member.Roles)
+	fmt.Printf("roles %+v\n", m.Message)
 	if containsVal(m.Message.Member.Roles, id) > -1 {
 		return true
 	}
