061ef655604312788422faf2156fb96837fcc8b5
Adam Siegel <tobioatom@gmail.com>
2021-03-11 14:51:14 -0800

    updating like function

--- dicebot/reaction.go
+++ dicebot/reaction.go
@@ -9,11 +9,16 @@ import (
 
 // Reaction reaction roles
 func Reaction(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
-	log.Printf("state %+v  message %+v\n", s, m.MessageReaction.UserID)
+	log.Printf("state %+v  message %+v\n", s, m.MessageReaction.MessageID)
 	mem, _ := s.GuildMember(m.GuildID, m.UserID)
 	mem2, _ := s.GuildMember(m.GuildID, m.MessageReaction.UserID)
-	log.Printf("Members %s %s\n ", mem.User.Username, mem2.User.Username)
+	om, _ := s.ChannelMessage(m.ChannelID, m.MessageID)
+	op, _ := s.GuildMember(m.GuildID, om.Author.ID)
+	log.Printf("Members %s %s %s\n ", mem.User.Username, mem2.User.Username, op.User.Username)
 	if m.UserID == s.State.User.ID {
+		return
+	}
+	if m.UserID == op.User.ID {
 		return
 	}
 	if val, ok := sessions[m.UserID]; ok {
